cb9df11fc836b651c501b4c3427dc6f89edba8ce
Hongwei Han <louis.niuniu@gmail.com>
2024-03-11 18:38:16 +0100

    add nacos function

--- order-service/src/main/java/org/example/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/example/orderservice/OrderServiceApplication.java
@@ -1,9 +1,11 @@
 package org.example.orderservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.example.orderservice.mapper")
 public class OrderServiceApplication {
 
     public static void main (String[] args) {
--- order-service/src/main/java/org/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/example/orderservice/controller/OrderController.java
@@ -1,5 +1,7 @@
 package org.example.orderservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.orderservice.config.PatternProperties;
 import org.example.orderservice.pojo.Order;
 import org.example.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +11,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 @RestController
 @RequestMapping("/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("prop")
+    public PatternProperties properties(){
+        return properties;
+    }
+
+    @GetMapping("now")
+    public String now(){
+        return LocalDateTime.now().format( DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
     @GetMapping("{id}")
     public void findById(@PathVariable Integer id){
         System.out.println(id );
