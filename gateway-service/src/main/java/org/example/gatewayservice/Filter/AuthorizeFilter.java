6bc2854d0f5f0053a60d6d5ee4a744362ee09b03
Hongwei Han <louis.niuniu@gmail.com>
2024-03-11 19:53:04 +0100

    add gateway function

--- gateway-service/src/main/java/org/example/gatewayservice/Filter/AuthorizeFilter.java
+++ gateway-service/src/main/java/org/example/gatewayservice/Filter/AuthorizeFilter.java
@@ -1,9 +1,8 @@
-package org.example.gateway;
+package org.example.gatewayservice.Filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -15,6 +14,7 @@ import reactor.core.publisher.Mono;
 public class AuthorizeFilter implements GlobalFilter, Ordered {
     @Override
     public Mono <Void> filter (ServerWebExchange exchange, GatewayFilterChain chain) {
+        // 1.获取请求参数
         ServerHttpRequest request = exchange.getRequest();
         MultiValueMap <String, String> params = request.getQueryParams();
         // 2.获取参数中的 authorization 参数
--- gateway-service/src/test/java/org/example/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/org/example/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.gateway;
+package org.example.gatewayservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads () {
+    }
 
 }
