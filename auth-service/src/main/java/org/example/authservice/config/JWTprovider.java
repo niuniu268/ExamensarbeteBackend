6cac78d00866ef9a0cde6e3f44322e0c0d71d81c
Hongwei Han <louis.niuniu@gmail.com>
2024-05-23 14:04:06 +0200

    update auth-service

--- auth-service/src/main/java/org/example/authservice/config/JWTprovider.java
+++ auth-service/src/main/java/org/example/authservice/config/JWTprovider.java
@@ -2,11 +2,13 @@ package org.example.authservice.config;
 
 
 import io.jsonwebtoken.*;
+import jakarta.annotation.PostConstruct;
 import org.example.authservice.pojo.UserPrincipal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 import io.jsonwebtoken.*;
@@ -22,16 +24,23 @@ import java.util.Date;
 @Component
 public class JWTprovider {
 
-//    @Value("${app.jwtSecret}")
-//    private String jwtSecret;
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
 
     @Value("${app.jwtExpirationInMs}")
     private int jwtExpirationInMs;
 
     private SecretKey key;
 
-    public JWTprovider() {
-        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+//    public JWTprovider() {
+//        this.key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+//    }
+
+    @PostConstruct
+    public void init() {
+
+        byte[] keyBytes = jwtSecret.getBytes( StandardCharsets.UTF_8);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
     public String generateToken(Authentication authentication) {
@@ -43,7 +52,7 @@ public class JWTprovider {
                 .setSubject(userPrincipal.getUsername())
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
-                .signWith(key, SignatureAlgorithm.HS512) // Use the secure key
+                .signWith(key) // Use the secure key
                 .compact();
     }
 
